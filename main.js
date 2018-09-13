8572f8967919dfe67326211772c5d46d6f1595c5
Pearl Law <pearllynnelaw@gmail.com>
2018-09-13 12:36:54 -0700

    Add a turn method to the car instance that updates its direction and its  class list. #2

--- main.js
+++ main.js
@@ -5,11 +5,19 @@ class Car {
     this.direction = direction
     this.location = location
   }
+
+  turn(direction) {
+    this.direction = direction
+    var img = document.querySelector('.car-img')
+    this.$img = img.classList.replace('car-img', 'rotated')
+  }
 }
 
 var pinkCar = document.createElement('img')
+pinkCar.classList.add('car-img')
 pinkCar.setAttribute('src', 'race-car.png')
-pinkCar.setAttribute('style', 'height: 20rem')
+pinkCar.setAttribute('style', 'height: 15rem')
+
 document.body.appendChild(pinkCar)
 
 var myCar = new Car(pinkCar, 5, 'north', '[0, 0]')
