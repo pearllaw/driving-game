e8ada6cba38dd494b281583cd30915a9d2bfa794
Pearl Law <pearllynnelaw@gmail.com>
2018-09-13 14:43:16 -0700

    Add a 'keydown' event listener for Arrow keys on the document that faces the car in the direction pressed. #2

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ class Car {
   turn(direction) {
     this.direction = direction
     var img = document.querySelector('.car-img')
-    this.$img = img.classList.replace('car-img', 'rotated')
+    this.$img = img.classList.toggle('rotated')
   }
 }
 
@@ -17,7 +17,23 @@ var pinkCar = document.createElement('img')
 pinkCar.classList.add('car-img')
 pinkCar.setAttribute('src', 'race-car.png')
 pinkCar.setAttribute('style', 'height: 15rem')
-
 document.body.appendChild(pinkCar)
 
 var myCar = new Car(pinkCar, 5, 'north', '[0, 0]')
+
+document.addEventListener('keydown', function(event) {
+  switch (event.key) {
+    case 'ArrowUp':
+      myCar.turn('north')
+      break
+    case 'ArrowRight':
+      myCar.turn('east')
+      break
+    case 'ArrowDown':
+      myCar.turn('south')
+      break
+    case 'ArrowLeft':
+      myCar.turn('west')
+      break
+  }
+})
